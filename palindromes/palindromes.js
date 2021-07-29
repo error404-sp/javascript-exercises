02204ab750fccd97b2cc72a1158de02fbd05c177
error404-sp <shreyasipatil404@gmail.com>
2021-07-30 00:16:45 +0530

    palindrome

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+   
+    processedString = str.toLowerCase().replace(/[^A-Za-z]/g, "");
+   for(let i=0;i<Math.round(processedString.length/2);i++){
+  
+        if(processedString[i] != processedString[processedString.length - i-1]){
+            return false;
+    }
+}
+return true;
 
+ 
 };
 
 module.exports = palindromes;
